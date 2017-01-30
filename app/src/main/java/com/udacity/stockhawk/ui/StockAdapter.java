7a061a54280ae7206b18868fda2a057c32cbef3f
KeerthanaS <keerukeerthana8@gmail.com>
2017-01-31 01:30:34 +0530

    Few Changes

---
+++ app/src/main/java/com/udacity/stockhawk/ui/StockAdapter.java
@@ -0,0 +1,161 @@
+package com.udacity.stockhawk.ui;
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract;
+import com.udacity.stockhawk.data.PrefUtils;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+class StockAdapter extends RecyclerView.Adapter<StockAdapter.StockViewHolder> {
+
+    private final Context context;
+    private final DecimalFormat dollarFormatWithPlus;
+    private final DecimalFormat dollarFormat;
+    private final DecimalFormat percentageFormat;
+    private Cursor cursor;
+    private final StockAdapterOnClickHandler clickHandler;
+
+    StockAdapter(Context context, StockAdapterOnClickHandler clickHandler) {
+        this.context = context;
+        this.clickHandler = clickHandler;
+
+        dollarFormat = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+        dollarFormatWithPlus = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+        dollarFormatWithPlus.setPositivePrefix("+$");
+        percentageFormat = (DecimalFormat) NumberFormat.getPercentInstance(Locale.getDefault());
+        percentageFormat.setMaximumFractionDigits(2);
+        percentageFormat.setMinimumFractionDigits(2);
+        percentageFormat.setPositivePrefix("+");
+    }
+
+    void setCursor(Cursor cursor) {
+        this.cursor = cursor;
+        notifyDataSetChanged();
+    }
+
+    String getSymbolAtPosition(int position) {
+
+        cursor.moveToPosition(position);
+        return cursor.getString(Contract.Quote.POSITION_SYMBOL);
+    }
+
+    @Override
+    public StockViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View item = LayoutInflater.from(context).inflate(R.layout.list_item_quote, parent, false);
+
+        return new StockViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(StockViewHolder holder, int position) {
+
+        cursor.moveToPosition(position);
+
+
+        holder.symbol.setText(cursor.getString(Contract.Quote.POSITION_SYMBOL));
+        holder.price.setText(dollarFormat.format(cursor.getFloat(Contract.Quote.POSITION_PRICE)));
+
+
+        float rawAbsoluteChange = cursor.getFloat(Contract.Quote.POSITION_ABSOLUTE_CHANGE);
+        float percentageChange = cursor.getFloat(Contract.Quote.POSITION_PERCENTAGE_CHANGE);
+
+        if (rawAbsoluteChange > 0) {
+            holder.change.setBackgroundResource(R.drawable.percent_change_pill_green);
+        } else {
+            holder.change.setBackgroundResource(R.drawable.percent_change_pill_red);
+        }
+
+        String change = dollarFormatWithPlus.format(rawAbsoluteChange);
+        String percentage = percentageFormat.format(percentageChange / 100);
+
+        if (PrefUtils.getDisplayMode(context)
+                .equals(context.getString(R.string.pref_display_mode_absolute_key))) {
+            holder.change.setText(change);
+            holder.setContentDescription(R.string.pref_display_mode_absolute_key);
+        } else {
+            holder.change.setText(percentage);
+            holder.setContentDescription(R.string.pref_display_mode_percentage_key);
+        }
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        int count = 0;
+        if (cursor != null) {
+            count = cursor.getCount();
+        }
+        return count;
+    }
+
+
+    interface StockAdapterOnClickHandler {
+        void onClick(String symbol);
+    }
+
+    class StockViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        View item;
+
+        @BindView(R.id.symbol)
+        TextView symbol;
+
+        @BindView(R.id.price)
+        TextView price;
+
+        @BindView(R.id.change)
+        TextView change;
+
+        StockViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+            item = itemView;
+        }
+
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            cursor.moveToPosition(adapterPosition);
+            int symbolColumn = cursor.getColumnIndex(Contract.Quote.COLUMN_SYMBOL);
+            clickHandler.onClick(cursor.getString(symbolColumn));
+
+        }
+
+        public void setContentDescription(int resId) {
+            String description;
+            if (resId == R.string.pref_display_mode_percentage_key) {
+                description = String.format(context.getString(R.string.item_description_percent),
+                        symbol.getText(),
+                        price.getText(),
+                        change.getText());
+            } else {
+                description = String.format(context.getString(R.string.item_description_absolute),
+                        symbol.getText(),
+                        price.getText(),
+                        change.getText());
+            }
+            item.setContentDescription(description);
+
+        }
+
+
+    }
+}
